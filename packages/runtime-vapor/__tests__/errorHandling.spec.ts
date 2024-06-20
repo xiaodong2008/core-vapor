65268432086973e8da6d40b93ec9e26023d6f6eb
xiaodong2008 <admin@xiaodong.moe>
2024-06-20 23:32:12 +0800

    in watch callback

--- packages/runtime-vapor/__tests__/errorHandling.spec.ts
+++ packages/runtime-vapor/__tests__/errorHandling.spec.ts
@@ -5,6 +5,8 @@ import { createComponent } from '../src/apiCreateComponent'
 import { makeRender } from './_utils'
 import { template } from '../src/dom/template'
 import { watch, watchEffect } from '../src/apiWatch'
+import { nextTick } from '../src/scheduler'
+import { ref } from '@vue/reactivity'
 
 const define = makeRender()
 
